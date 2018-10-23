2e686d1abeb931ca2410a14051ef4b5fb8e89b84
lawfulstupid <a.bradley.dm@gmail.com>
2018-10-23 21:40:21 +0100

    Intergrated ParserUtils into AbLib

---
+++ NameNum.hs
@@ -0,0 +1,154 @@
+-- This module provides functions to give full names to symbolic numbers.
+-- Uses the Conway-Weschler naming scheme.
+module NumberName.NameNum where
+
+import AbLib.Data.List ((!?))
+import Data.Maybe (fromJust, listToMaybe, catMaybes)
+import Control.Applicative (liftA2)
+
+-- DIGIT := one | two | three | four | five | six | seven | eight | nine
+-- TEENS := ten | eleven | twelve | thirteen | fourteen | fifteen | sixteen | seventeen | eighteen | nineteen
+-- TENS  := twenty | thirty | forty | fifty | sixty | seventy | eighty | ninety
+
+-- D2 := DIGIT | TEENS | TENS | TENS DIGIT
+-- NT := and D2 | ""
+
+-- D3 := D2 | DIGIT hundred NT
+
+-- T0 := D3
+-- T1 := T0 | D3 thousand T0 | D3 thousand NT
+-- T2 := T1 | D3 million T1  | D3 million NT
+-- T3 := T2 | D3 billion T2  | D3 billion NT
+-- et cetera
+
+{- Gives the full name of an Integral value. -}
+nameInt :: Integral a => a -> String
+nameInt n = case compare n 0 of
+   LT -> "negative" ++ nameInt (negate n)
+   EQ -> "zero"
+   GT -> fromJust $ build $ split n
+   where
+
+   {- Concatenates Maybe Strings. -}
+   (<<) :: Maybe String -> Maybe String -> Maybe String
+   (<<) = liftA2 (++)
+
+   {- Return the first `Just` in a list. Nothing if there are none.
+    - Condenses structures of the form
+         `if checkA then valueA else if checkB then valueB else ...`
+      to `first [safeCheckA, safeCheckB, ...]` -}
+   first :: [Maybe a] -> Maybe a
+   first = listToMaybe . catMaybes
+
+   {- Splits a number into groups of 3 digits in descending order.
+    - Last group contains unit, tens, and hundreds digits. -}
+   split :: Integral a => a -> [a]
+   split 0 = []
+   split n = let
+      (d,m) = divMod n 1000
+      in (split d) ++ [m] 
+
+   {- Names a grouped number. -}
+   build :: Integral a => [a] -> Maybe String
+   build (n:[]) = digit3 n -- Final three digits
+   build (0:ns) = build ns -- Ignore leading zero groups
+   build (n:ns) = let
+      ord = length $ tail $ ns                         -- Order (large number) of current group
+      cur = digit3 n << Just (" " ++ nameLarge ord)    -- Full name of current group
+      in cur << first [term ns, Just ", " << build ns] -- Check if only 2 digits left (requires "and")
+
+   {- Names a 2-digit grouped number, assuming it is the final 2 digits of a larger number. -}
+   term :: Integral a => [a] -> Maybe String
+   term (0:[]) = Just ""                  -- All groups are zero
+   term (n:[]) = Just " and " << digit2 n -- Last group is nonzero
+   term (0:ns) = term ns                  -- Ignore leading zero groups
+   term _      = Nothing                  -- Empty lists or more than 2 digits
+
+   {- Names a 3-digit number. -}
+   digit3 :: Integral a => a -> Maybe String
+   digit3 n = let
+      (d,m) = divMod n 100
+      in first [digit2 n, digit d << Just " hundred" << term [m]]
+
+   {- Names a 2-digit number. -}
+   digit2 :: Integral a => a -> Maybe String
+   digit2 n = let
+      m = mod n 10
+      in first [digit n, teens n, tens n, tens (n - m) << Just " " << digit m]
+
+   {- Names a strict multiple of 10. -}
+   tens :: Integral a => a -> Maybe String
+   tens = flip lookup
+      [ (20,"twenty"), (30,"thirty"),  (40,"forty"),  (50,"fifty")
+      , (60,"sixty"),  (70,"seventy"), (80,"eighty"), (90,"ninety") ]
+
+   {- Names a -teen number. -}
+   teens :: Integral a => a -> Maybe String
+   teens x = lookup x
+      [ (10,"ten"), (11,"eleven"), (12,"twelve"), (13,"thir"), (14,"four")
+      , (15,"fif"), (16,"six"),    (17,"seven"),  (18,"eigh"), (19,"nine") ]
+      << (Just $ if elem x [10..12] then "" else "teen") -- Perhaps excessive shortcutting
+
+   {- Names a single digit. -}
+   digit :: Integral a => a -> Maybe String
+   digit = flip lookup
+      [ (1,"one"),   (2,"two"),   (3,"three")
+      , (4,"four"),  (5,"five"),  (6,"six")
+      , (7,"seven"), (8,"eight"), (9,"nine") ]
+
+{- Names 1000^(n+1) -}
+nameLarge :: Integral a => a -> String
+nameLarge n | n < 0 = error "positive values only"
+nameLarge 0 = "thousand"         -- Special case (see note below)
+nameLarge n = nameLarge' n where -- Direct call to auxiliary (see note below)
+{- Special case prevents 0 -> "nillion".
+ - Auxiliary prevents 1000 -> "millithousand", etc. (should be millinillion).
+ - If we allow 1000 to be called "one nillion", both workarounds can be removed.
+ - I vote in favour of this move. -}
+
+   nameLarge' :: Integral a => a -> String
+   nameLarge' n = case prefix 0 n of
+      "" | n < 1000 -> assemble $ build 1 n
+      "" -> (init . init) (nameLarge' $ div n 1000) ++ (nameLarge' $ mod n 1000)
+      pf -> pf ++ "illion"
+         
+   build :: Integral a => a -> a -> [String]
+   build 4 _ = []
+   build d n = (prefix d $ mod n 10) : (build (d+1) $ div n 10)
+
+   {- Returns everything before the "illion". -}
+   prefix :: Integral a => a -> a -> String
+   prefix n k = maybe "" id $ table !? n' >>= (!? k')
+      where
+      n' = fromInteger $ toInteger n   -- convert to Int
+      k' = fromInteger $ toInteger k
+      table =
+         [ [ "n", "m",       "b",            "tr"
+           , "quadr",        "quint",        "sext"
+           , "sept",         "oct",          "non"         ]
+         , [ "", "un",       "duo",          "tre"
+           , "quattuor",     "quin",         "se"
+           , "septe",        "octo",         "nove"        ]
+         , [ "", "deci",     "viginti",      "triginta"
+           , "quadraginta",  "quinquaginta", "sexaginta"
+           , "septuaginta",  "octoginta",    "nonaginta"   ]
+         , [ "", "centi",    "ducenti",      "trecenti"
+           , "quadringenti", "quingenti",    "sescenti"
+           , "septingenti",  "octingenti",   "nongenti"    ] ]
+   
+   {- Puts all the elements together. -}
+   assemble :: [String] -> String
+   assemble [a,b,c] = assemble [a, b++c]
+   assemble [a,b] = init (a ++ extra a (head b) ++ b) ++ "illion" where
+      {- Extra Latin grammatical suffix rules. -}
+      extra :: String -> Char -> String
+      extra "se" c
+         | any (c==) "qtv" = "s"
+         | any (c==) "oc"  = "x"
+      extra "tre" c
+         | extra "se" c /= "" = "s"
+      extra "nove" c
+         | any (c==) "cdqst" = "n"
+         | any (c==) "ov" = "m"
+      extra "septe" c = extra "nove" c
+      extra _ _ = ""
---
+++ ParseNum.hs
@@ -0,0 +1,162 @@
+-- This module provides functions for decoding Strings describing numbers into number types.
+-- Uses the Conway-Wechsler naming scheme.
+module NumberName.ParseNum where
+
+import AbLib.Data.Parser
+import NumberName.NameNum
+import Data.List ((\\), isSuffixOf)
+import Data.Char (toLower)
+
+-- DIGIT := one | two | three | four | five | six | seven | eight | nine
+-- TEENS := ten | eleven | twelve | thirteen | fourteen | fifteen | sixteen | seventeen | eighteen | nineteen
+-- TENS  := twenty | thirty | forty | fifty | sixty | seventy | eighty | ninety
+
+-- D2 := DIGIT | TEENS | TENS | TENS DIGIT
+-- NT := and D2 | ""
+
+-- D3 := D2 | DIGIT hundred NT
+
+-- T0 := D3
+-- T1 := T0 | D3 thousand T0 | D3 thousand NT
+-- T2 := T1 | D3 million T1  | D3 million NT
+-- T3 := T2 | D3 billion T2  | D3 billion NT
+-- et cetera
+
+{- Create Integral from a full name. -}
+parseInt :: Integral a => String -> a
+parseInt = parse intParser
+   . substitute [(",",""), ("illions","illion"), ("thousands","thousand")]
+   . map toLower
+   where
+
+   {- Parser for all integer values. -}
+   intParser :: Integral a => Parser a
+   intParser = parallel
+      [ match "negative" & spaces & intParser ~> negate  -- negatives
+      , match "zero" ~> const 0                          -- zero
+      , \s -> posParser (getOrder s) s                 ] -- positives
+   
+   {- Parser for integers strictly between 0 and 1000^(n+2). -}
+   posParser :: Integral a => a -> Parser a
+   posParser 0 = let
+      name = match "thousand" <> match "nillion"         -- generosity
+      this = digit3 <& spaces <& name ~> (* 1000)
+      next = accept 0 <> term <> (spaces & digit3)
+      in (this <&> next ~> uncurry (+)) <> digit3
+   posParser n = let
+      factor = 1000 ^ (n + 1)
+      this = digit3 <& spaces <& match (nameLarge n) ~> (* factor)
+      next = accept 0 <> term <> (spaces & posParser (n-1))
+      in (this <&> next ~> uncurry (+)) <> posParser (n-1)
+   
+   {- Parser for symbolic representation of numbers. -}
+   sym :: (Integral a, Integral b, Show a) => [a] -> Parser b
+   sym ns = options [(show n, fromInteger $ toInteger n) | n <- ns]
+      -- need to convert between Integral types to avoid Show constraint.
+   
+   {- Parser for names of numbers. Practical only for small-scale. -}
+   parseExact :: (Integral a) => [a] -> Parser a
+   parseExact ns = options [(nameInt n, n) | n <- ns]
+   
+   {- Parser for all forms of a 3-digit number. -}
+   digit3 :: Integral a => Parser a
+   digit3 = let
+      hun = digit1 <& spaces <& match "hundred" ~> (100*)   -- match "`x` hundred" as 100*x
+      -- Parser enumerates possible forms of a 3-digit number:
+      in parallel
+         [ sym [100..999]                 -- symbolic representation
+         , digit2                         -- 2-digit number
+         , hun <&> term ~> uncurry (+) ]  -- full three-digit number
+      
+   {- Parser for final two digits non-inital digits. -}
+   term :: Integral a => Parser a
+   term = (optional (spaces & match "and") & spaces & digit2) <> accept 0
+   
+   {- Parser for all forms of a 2-digit number. -}
+   digit2 :: Integral a => Parser a
+   digit2 = let
+      tens = parseExact [20,30..90]       -- match strict multiples of ten
+      join = space <> match "-"
+      unit = accept 0 <> (join & digit1)
+      -- Parser enumerates possible forms of a 2-digit number:
+      in parallel
+         [ sym [10..99]                   -- symbolic representation
+         , digit1                         -- 1-digit number
+         , parseExact [10..19]            -- teen number
+         , tens <&> unit ~> uncurry (+) ] -- "`x`ty `y`"
+   
+   {- Parser for all forms of a 1-digit number. -}
+   digit1 :: Integral a => Parser a
+   digit1 = sym [1..9] <> parseExact [1..9]
+   
+   {- Computes the order of a named number by finding it's first "zillion". -}
+   getOrder :: Integral a => String -> a
+   getOrder s = case filter (isSuffixOf "illion") (words s) of
+      [ ] -> 0
+      [x] -> parseLarge x
+   
+-- Parser for large number orders (illion).
+-- Inverse of nameLarge.
+parseLarge :: Integral a => String -> a
+parseLarge = parse ((match "thousand" ~> const 0) <> largeParser) . map toLower
+   where
+
+   {- Parser for large numbers. -}
+   largeParser :: Integral a => Parser a
+   largeParser = initial <&> largeParser' ~> \(x,f) -> f x
+      where
+      initial :: Integral a => Parser a
+      initial = prefix <& match "lli" ~> (*1000)
+      largeParser' :: Integral a => Parser (a -> a)
+      largeParser' = parallel
+         [ match "on"               ~> const (`div` 1000)
+         , initial <&> largeParser' ~> \ (x,f) z -> f (1000*z+x) ]
+   
+   {- Prefixes for large 0-999. -}
+   prefix :: Integral a => Parser a
+   prefix = let
+      prefix0 = options $ zip (table !! 0) [0..9]
+      prefix2 = options $ zip (table !! 2) [10,20..90]
+      prefix3 = options $ zip (table !! 3) [100,200..900]
+      combine = prefix2 <&> prefix3 ~> uncurry (+)
+      in prefix0
+      <> (prefix1 <&> (prefix2 <> prefix3 <> combine) ~> uncurry (+))
+   
+   {- Unit prefixes for large 10-999. The units part is more involved, hence this function. -}
+   prefix1 :: Integral a => Parser a
+   prefix1 = let
+      abc  = ['a'..'z']
+      no36 = abc \\ "coqtv"
+      no79 = abc \\ "cdqstov"
+      in parallel
+         [ accept 0
+         , match "un"                                 ~> const 1
+         , match "duo"                                ~> const 2
+         , match "tres"   & lookAhead (oneOf "coqtv") ~> const 3
+         , match "tre"    & lookAhead (oneOf no36)    ~> const 3
+         , match "quattuor"                           ~> const 4
+         , match "quin"                               ~> const 5
+         , match "sex"    & lookAhead (oneOf "co")    ~> const 6
+         , match "ses"    & lookAhead (oneOf "qtv")   ~> const 6
+         , match "se"     & lookAhead (oneOf no36)    ~> const 6
+         , match "septen" & lookAhead (oneOf "cdqst") ~> const 7
+         , match "septem" & lookAhead (oneOf "ov")    ~> const 7
+         , match "septe"  & lookAhead (oneOf no79)    ~> const 7
+         , match "octo"                               ~> const 8
+         , match "noven"  & lookAhead (oneOf "cdqst") ~> const 9
+         , match "novem"  & lookAhead (oneOf "ov")    ~> const 9
+         , match "nove"   & lookAhead (oneOf no79)    ~> const 9 ]
+   
+   {- Simple table of prefixes. -}
+   table :: [[String]]
+   table =
+      [ [ "ni", "mi",     "bi",           "tri"
+        , "quadri",       "quinti",       "sexti"
+        , "septi",        "octi",         "noni"      ]
+      , [ {- This row is intentionally left empty. -} ]
+      , [ "deci",         "viginti",      "triginta"
+        , "quadraginta",  "quinquaginta", "sexaginta"
+        , "septuaginta",  "octoginta",    "nonaginta" ]
+      , [ "centi",        "ducenti",      "trecenti"
+        , "quadringenti", "quingenti",    "sescenti"
+        , "septingenti",  "octingenti",   "nongenti"  ] ]
