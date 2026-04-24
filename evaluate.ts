c0db2aa320ccfc1cb1dc27d763e060dc1a04bbe2
Alex Bradley <a.bradley.dm@gmail.com>
2026-04-24 02:39:19 +0100

    Expression parser

---
+++ evaluate.ts
@@ -0,0 +1,151 @@
+function evaluate(str: string): bigint {
+  str = str.replace(/\s+/g, '');
+  const result = exprParser.run(str);
+  console.log(result);
+  return 0n; // WIP
+}
+
+class ExprTree {
+  constructor(
+    private expr1: ExprTree | bigint,
+    private op: Op,
+    private expr2: ExprTree | bigint
+  ) {}
+}
+
+type Expr = ExprTree | bigint;
+type Op = '+' | '-' | '*' | '/' | '%' | '^';
+
+type Result<T> = { value: T, remainder: string } | null;
+
+class Parser<T> {
+
+  constructor(public run: (str: string) => Result<T>) {}
+
+  static pure<T>(value: T | Parser<T>): Parser<T> {
+    if (value instanceof Parser) return value;
+    return new Parser(str => ({ value, remainder: str }));
+  }
+
+  static match(...values: string[]): Parser<string> {
+    return new Parser(str => {
+      for (const value of values) {
+        if (str.startsWith(value)) {
+          return { value, remainder: str.slice(value.length) }
+        }
+      }
+      return null;
+    });
+  }
+
+  next<S>(getNextParser: (token: T) => S | Parser<S>, getFailover?: () => S | Parser<S>): Parser<S> {
+    return new Parser(str => {
+      const result = this.run(str);
+      if (result !== null) {
+        const nextParser = Parser.pure(getNextParser(result.value));
+        return nextParser.run(result.remainder);
+      } else if (getFailover) {
+        const nextParser = Parser.pure(getFailover());
+        return nextParser.run(str);
+      } else {
+        return null;
+      }
+    });
+  }
+}
+
+const integerParser: Parser<bigint> = new Parser(str => {
+  if (!/^-?[0-9]+/.test(str)) return null;
+  const splits = str.split(/(?<=^-?[0-9]+)(?=[^0-9])/);
+  return {
+    value: BigInt(splits[0]),
+    remainder: splits[1] || ''
+  }
+});
+
+const numberParser: Parser<bigint> = integerParser.next((mantissa: bigint) => {
+  return Parser.match('e', 'E').next(() => {
+    return integerParser.next((exponent: bigint) => {
+      return mantissa * (10n ** exponent)
+    })
+  }, () => mantissa);
+});
+
+const op1Parser: Parser<Op> = Parser.match('^').next(op => {
+  switch (op) {
+    case '^': return '^';
+    default: throw new Error();
+  }
+});
+
+const op2Parser: Parser<Op> = Parser.match('*', '/', '%').next(op => {
+  switch (op) {
+    case '*': return '*';
+    case '/': return '/';
+    case '%': return '%';
+    default: throw new Error();
+  }
+});
+
+const op3Parser: Parser<Op> = Parser.match('+', '-').next(op => {
+  switch (op) {
+    case '+': return '+';
+    case '-': return '-';
+    default: throw new Error();
+  }
+});
+
+const precParsers: Array<Parser<Expr>> = [
+  Parser.match('(').next(() => {
+    return precParsers[3].next(prec3 => {
+      return Parser.match(')').next(() => {
+        return prec3;
+      })
+    })
+  }, () => numberParser),
+  Parser.match('').next(() => {
+    return precParsers[0].next(expr1 => {
+      return op1Parser.next(op => {
+        return precParsers[1].next(expr2 => {
+          return new ExprTree(expr1, op, expr2);
+        });
+      }, () => expr1);
+    })
+  }),
+  Parser.match('').next(() => {
+    return precParsers[1].next(expr1 => {
+      return op2Parser.next(op => {
+        return precParsers[2].next(expr2 => {
+          return new ExprTree(expr1, op, expr2);
+        })
+      }, () => expr1)
+    })
+  }),
+  Parser.match('').next(() => {
+    return precParsers[2].next(expr1 => {
+      return op3Parser.next(op => {
+        return precParsers[3].next(expr2 => {
+          return new ExprTree(expr1, op, expr2);
+        })
+      }, () => expr1)
+    })
+  })
+]
+
+const exprParser: Parser<Expr> = precParsers[3];
+
+/*
+<expr> ::= <prec3>
+<prec3> ::= <prec2> <op3> <prec3> | <prec2>
+<op3> ::= '+' | '-'
+<prec2> ::= <prec1> <op2> <prec2> | <prec1>
+<op2> ::= '*' | '/' | '%'
+<prec1> ::= <prec0> '^' <prec1> | <prec0>
+<prec0> ::= '(' <expr> ')' | <number>
+
+<number> ::= <integer> | <integer><e><integer>
+<e> ::= 'e' | 'E'
+<integer> ::= <digits> | -<digits>
+<digits> ::= <digit> | <digit><digits>
+<digit> ::= '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+*/
